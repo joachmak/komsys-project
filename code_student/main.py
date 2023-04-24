97616c728a548881b15b85f2660439502dc2c63d
joachmak <joachimmaksim@gmail.com>
2023-04-24 15:17:36 +0200

    feat: save tab position

--- code_student/main.py
+++ code_student/main.py
@@ -26,26 +26,15 @@ class UserInterface:
         self.mqtt_client = MQTTClient()
         self.current_scene = -1
         self.modules = modules
-        self.start_app()
         self.logged_in_user = ""
-        self.selected_module = None
-        self.selected_task = None
+        self.selected_module = 1
+        self.selected_task = 1
+        self.start_app()
+
 
     def show_error(self, message: str):
         self.app.setLabel(elems.LAB_ERROR, message)
         self.app.showLabel(elems.LAB_ERROR)
-
-    def on_login_click(self):
-        """ Try logging in """
-        group_name = self.app.getEntry(elems.TXT_GROUP_NAME)
-        if group_name != "":
-            if not self.mqtt_client.login(group_name):
-                self.show_error("")
-            self.logged_in_user = group_name
-            # TODO: do some MQTT stuff ?
-            self.show_scene(Scene.MAIN_PAGE)
-            return
-        self.show_error("Error: Invalid group name")
 
     def start_app(self):
         """ Set up initial scene """
@@ -68,12 +57,6 @@ class UserInterface:
 
         self.app.topLevel.geometry(f"{x}x{y}+{window_x}+{window_y}")
 
-    def on_task_click(self, btn_text: str):
-        """ Set selected task and module, and switch scene to help request view """
-        self.selected_task = int(btn_text.split(" ")[2]) - 1
-        self.selected_module = int(btn_text.split(" ")[0][1:])
-        self.show_scene(Scene.HELP_REQUEST)
-
     def show_scene(self, scene: int):
         def add_side_menu(back_btn_func=None):
             self.app.startLabelFrame("Menu", row=0, rowspan=5, column=0, colspan=1)
@@ -82,14 +65,25 @@ class UserInterface:
                 self.app.addLink("Back", back_btn_func)
             self.app.addLink("Log out", lambda x: self.show_scene(Scene.LOGIN))
             if back_btn_func is None:
-                self.app.addLabel(f"---")
-            self.app.addLabel(f"--")
-            self.app.addLabel(f"-")
+                self.app.addLabel(f"MENU_WHITESPACE_1", text="")
+            self.app.addLabel(f"MENU_WHITESPACE_2", text="")
+            self.app.addLabel(f"MENU_WHITESPACE_3", text="")
             self.app.stopLabelFrame()
 
         self.app.removeAllWidgets()
         self.current_scene = scene
         if scene == Scene.LOGIN:
+            def on_login_click():
+                """ Try logging in """
+                group_name = self.app.getEntry(elems.TXT_GROUP_NAME)
+                if group_name != "":
+                    if not self.mqtt_client.login(group_name):
+                        self.show_error("")
+                    self.logged_in_user = group_name
+                    # TODO: do some MQTT stuff ?
+                    self.show_scene(Scene.MAIN_PAGE)
+                    return
+                self.show_error("Error: Invalid group name")
 
             self.set_window_size_and_center(500, 100)
             self.app.startLabelFrame("Start")
@@ -99,10 +93,16 @@ class UserInterface:
             self.app.addLabel(elems.LAB_ERROR, "Error, something went wrong", 1, 1)
             self.app.setLabelFg(elems.LAB_ERROR, "red")
             self.app.hideLabel(elems.LAB_ERROR)
-            self.app.addButton(elems.BTN_LOGIN, self.on_login_click, 2, 1)
+            self.app.addButton(elems.BTN_LOGIN, on_login_click, 2, 1)
             self.app.stopLabelFrame()
 
         elif scene == Scene.MAIN_PAGE:
+            def on_task_click(btn_text: str):
+                """ Set selected task and module, and switch scene to help request view """
+                self.selected_task = int(btn_text.split(" ")[2]) - 1
+                self.selected_module = int(btn_text.split(" ")[0][1:])
+                self.show_scene(Scene.HELP_REQUEST)
+
             self.set_window_size_and_center(500, 250)
             add_side_menu()
             self.app.startLabelFrame("Modules", sticky="news", row=0, rowspan=5, column=1, colspan=3)
@@ -118,17 +118,28 @@ class UserInterface:
                         col = 0
                         row += 1
                     # OBS: if you change the text, you must also update the parsing in the self.on_task_click method
-                    self.app.addLink(f"M{module.number} task {i+1}", self.on_task_click, column=col, row=row)
+                    self.app.addLink(f"M{module.number} task {i+1}", on_task_click, column=col, row=row)
                 self.app.stopTab()
             self.app.stopTabbedFrame()
+            self.app.setTabbedFrameSelectedTab("ModuleTabFrame", str(self.selected_module))
             self.app.stopLabelFrame()
         elif scene == Scene.HELP_REQUEST:
+            def on_submit():
+                # TODO: mqtt stuff
+                print("sending mqtt request")
+                self.app.setLabel("LAB_SENT_STATUS", "Request status: SENT")
+                self.app.setLabelFg("LAB_SENT_STATUS", "orange")
+                self.app.setButton("BTN_SUBMIT", "Update help request")
+
             add_side_menu(lambda x: self.show_scene(Scene.MAIN_PAGE))
-            self.app.startLabelFrame(f"Request help with module {self.selected_module} task {self.selected_task + 1}", sticky="news", row=0, rowspan=6, column=1, colspan=3)
+            self.app.startLabelFrame(f"Request help with module {self.selected_module} task {self.selected_task + 1}",
+                                     sticky="news", row=0, rowspan=6, column=1, colspan=3)
             self.app.addCheckBox("Online")
             self.app.addTextArea("Comment", text="What do you need help with?")
-            self.app.addButton("Send help request", lambda x: print("Send request..."))
-            self.app.addLabel("Request status: NOT SENT")
+            self.app.addButton("BTN_SUBMIT", on_submit)
+            self.app.setButton("BTN_SUBMIT", "Send help request")
+            self.app.addLabel("LAB_SENT_STATUS", text="Request status: NOT SENT")
+            self.app.setLabelFg("LAB_SENT_STATUS", "red")
             self.app.stopLabelFrame()
         else:
             pass
