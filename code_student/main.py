382c5a47d28693917baf86022fed0af721342056
joachmak <joachimmaksim@gmail.com>
2023-04-24 17:42:52 +0200

    feat: only permit 1 help request at a time

--- code_student/main.py
+++ code_student/main.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
 from appJar import gui
 import code_student.gui_elems as elems
 from enum import Enum
 from uuid import uuid1
 
 from code_student.module import Module
+from code_student.request_form import HelpRequest
 
 
 class MQTTClient:
@@ -32,8 +35,8 @@ class UserInterface:
         self.logged_in_user = ""
         self.selected_module = 1
         self.selected_task = 1
+        self.active_help_request: Optional[HelpRequest] = None
         self.start_app()
-
 
     def show_error(self, message: str):
         self.app.setLabel(elems.LAB_ERROR, message)
@@ -68,6 +71,10 @@ class UserInterface:
                 self.app.addLabel(str(uuid1()), text="", row=_i + current_row_count + 1, rowspan=1)
 
         def add_side_menu(back_btn_func=None, desired_rows=5):
+            def show_active_request():
+                self.selected_task = self.active_help_request.task_idx
+                self.selected_module = self.active_help_request.module_number
+                self.show_scene(Scene.HELP_REQUEST)
             self.app.startLabelFrame("Menu", row=0, rowspan=5, column=0, colspan=1)
             self.app.addLabel(f"Logged in as {self.logged_in_user}")
             current_row_count = 1
@@ -75,7 +82,9 @@ class UserInterface:
                 current_row_count = 2
                 self.app.addLink("Back", back_btn_func)
             self.app.addLink("Log out", lambda x: self.show_scene(Scene.LOGIN))
-
+            if self.active_help_request is not None:
+                self.app.addLink("Show active request", show_active_request)
+                current_row_count += 1
             add_whitespace(desired_rows, current_row_count)
             self.app.stopLabelFrame()
 
@@ -134,24 +143,43 @@ class UserInterface:
             self.app.stopTabbedFrame()
             self.app.setTabbedFrameSelectedTab("ModuleTabFrame", str(self.selected_module))
             self.app.stopLabelFrame()
+
         elif scene == Scene.HELP_REQUEST:
             def on_help_submit():
-                # TODO: mqtt stuff
-                print("sending mqtt request")
-                self.app.setLabel("LAB_SENT_STATUS", "Request status: SENT")
-                self.app.setLabelFg("LAB_SENT_STATUS", "orange")
-                self.app.setButton("BTN_SUBMIT", "Update help request")
+                if self.active_help_request is None:
+                    # Send new help request
+                    comment = self.app.getTextArea("LAB_COMMENT")
+                    is_online = self.app.getCheckBox("Online")
+                    self.active_help_request = HelpRequest(self.selected_module, self.selected_task, is_online, comment)
+                    # TODO: mqtt stuff
+                    print("sending mqtt request")
+                else:
+                    # Cancel help request
+                    # TODO: mqtt stuff
+                    print("sending mqtt request")
+                    self.active_help_request = None
+                self.show_scene(Scene.HELP_REQUEST)
 
             add_side_menu(lambda x: self.show_scene(Scene.TASK_MENU))
             self.app.startLabelFrame(f"Request help with module {self.selected_module} task {self.selected_task + 1}",
                                      sticky="news", row=0, rowspan=6, column=1, colspan=3)
             self.app.addCheckBox("Online")
-            self.app.addTextArea("Comment", text="What do you need help with?")
+            self.app.addTextArea("LAB_COMMENT", text="")
             self.app.addButton("BTN_SUBMIT", on_help_submit)
             self.app.setButton("BTN_SUBMIT", "Send help request")
             self.app.addLabel("LAB_SENT_STATUS", text="Request status: NOT SENT")
-            self.app.setLabelFg("LAB_SENT_STATUS", "red")
+            if self.active_help_request is None:
+                self.app.setTextArea("LAB_COMMENT", text="What do you need help with?")
+                self.app.setLabel("LAB_SENT_STATUS", text="Request status: NOT SENT")
+                self.app.setLabelFg("LAB_SENT_STATUS", "red")
+            else:
+                self.app.setTextArea("LAB_COMMENT", text=self.active_help_request.comment)
+                self.app.setCheckBox("Online", ticked=self.active_help_request.is_online)
+                self.app.setLabel("LAB_SENT_STATUS", text="Request status: SENT")
+                self.app.setButton("BTN_SUBMIT", "Cancel help request")
+                self.app.setLabelFg("LAB_SENT_STATUS", "green")
             self.app.stopLabelFrame()
+
         elif scene == Scene.MARK_TASK_AS_DONE:
             def on_task_done_submit():
                 # TODO: mqtt stuff
@@ -172,9 +200,18 @@ class UserInterface:
             self.app.setButton("BTN_SUBMIT", "Mark as done")
             self.app.addLabel("LAB_DONE_STATUS", text="Status: NOT DONE")
             self.app.setLabelFg("LAB_DONE_STATUS", "red")
-
             self.app.stopLabelFrame()
+
         elif scene == Scene.TASK_MENU:
+            def has_selected_task_an_active_request() -> bool:
+                if self.active_help_request is None:
+                    return False
+                if self.selected_task != self.active_help_request.task_idx:
+                    return False
+                if self.selected_module != self.active_help_request.module_number:
+                    return False
+                return True
+
             self.set_window_size_and_center(500, 250)
             add_side_menu(lambda x: self.show_scene(Scene.MAIN_PAGE))
             self.app.startLabelFrame(f"Options for module {self.selected_module} task {self.selected_task + 1}",
@@ -183,7 +220,16 @@ class UserInterface:
             self.app.addButton("BTN_REQUEST_HELP", lambda x: self.show_scene(Scene.HELP_REQUEST))
             self.app.setButton("BTN_MARK_AS_DONE", "Mark task as done")
             self.app.setButton("BTN_REQUEST_HELP", "Send help request")
-            add_whitespace(5, 2)
+            if self.active_help_request is not None and not has_selected_task_an_active_request():
+                self.app.disableButton("BTN_REQUEST_HELP")
+                self.app.addLabel("LAB_ACTIVE_REQUEST", text="You can only have 1 active request at a time") \
+                    .config(foreground="red")
+                add_whitespace(5, 3)
+            elif has_selected_task_an_active_request():
+                self.app.setButton("BTN_REQUEST_HELP", "See active help request")
+                add_whitespace(5, 2)
+            else:
+                add_whitespace(5, 2)
             self.app.stopLabelFrame()
         else:
             pass
