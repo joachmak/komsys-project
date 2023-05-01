57ae5d2eda51cbab565a4eb029330e964c0e0e64
Julie Høgetveit <juho1410@gmail.com>
2023-05-01 11:20:15 +0200

    feat: TA state machines

---
+++ code_teaching_assistant/stm_utils.py
@@ -0,0 +1,64 @@
+
+def get_stm_transitions():
+    t0 = {
+        "source": "initial",
+        "target": "unclaimed"
+    }
+    t1 = {
+        "source": "unclaimed",
+        "target": "waiting",
+        "trigger": "claim_button"
+    }
+    t2 = {
+        "source": "waiting",
+        "target": "unclaimed",
+        "trigger": "t",
+        "effect": "timer_expired"
+    }
+    t3 = {
+        "source": "waiting",
+        "target": "claimed",
+        "trigger": "sig_acc_claim"
+    }
+    t4 = {
+        "source": "claimed",
+        "target": "unclaimed",
+        "trigger": "resolve_button"
+    }
+    t5 = {
+        "source": "waiting",
+        "target": "unclaimed",
+        "trigger": "cancel_claim"
+    }
+    t6 = {
+        "source": "waiting",
+        "target": "unclaimed",
+        "trigger": "resolve_button"
+    }
+    return [t0, t1, t2, t3, t4, t5, t6]
+
+
+def get_stm_states():
+    unclaimed = {
+        "name": "unclaimed",
+        "entry": "stm_log('unclaimed');"
+    }
+    waiting = {
+        "name": "waiting",
+        "entry": "stm_log('waiting'); start_timer('t', 5000); task_claimed"
+    }
+    claimed = {
+        "name": "claimed",
+        "entry": "stm_log('claimed')",
+        "exit": "request_resolved"
+    }
+    return [unclaimed, waiting, claimed]
+
+
+"""
+TODO: Fix funksjonene:
+- task_claimed
+- request_resolved
+- claim_button
+- resolve_button
+"""
